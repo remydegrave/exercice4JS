c7bf9268be988a5ea3a7d01903226a90333ebb65
remydegrave <bolieve.fr@gmail.com>
2020-11-03 14:20:58 +0100

    correction

--- assets/js/fichierscript.js
+++ assets/js/fichierscript.js
@@ -1,7 +1,7 @@
-document.getElementById('clickButton').onclick = showAlert;
-function showAlert() {
-var lastName = document.getElementById('lastName').value;
-var firstName = document.getElementById('firstName').value;
-var city = document.getElementById('city').value;
-alert('Bonjour, ' + lastName + ' ' + firstName + ' ' + 'de' + ' ' + city)
-}
+document.getElementById('clickButton').onclick = showAlert; // apl de la fonction showalert au clic de clickbutton
+function showAlert(){ // declaration de la fonction showalert
+     var lastname = document.getElementById('lastname').value; // récupération du nom
+     var firstname = document.getElementById('firstname').value; //récupération du prénom
+     var city = document.getElementById('city').value;//récupération de la ville
+     alert('nom : ' + lastname + '\nprenom : '+ firstname+ '\nville:'+ city);  // affichage de la boite de dialogue
+ }
